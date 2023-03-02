d2dba54181e1d2e19d916e9bdf5e26b07f5654a5
goldendev601 <phpdev126@gmail.com>
2023-03-03 00:30:54 +0800

    updated api to get expension payout response

--- api/app.js
+++ api/app.js
@@ -19,17 +19,61 @@ app.post("/payouts", jsonParser, (req, res, next) => {
   let expenses = [];
   let sum = 0;
   let equalShare = 0;
+  let owes = [];
+  let owed = [];
+  const payouts = [];
   if (data) {
     expenses = data.expenses;
-    expenses.forEach(async (expense) => {
-      sum += parseInt(expense.amount);
-    });
-    equalShare = sum / expenses.length;
+    expenses_by_person = {};
+    for (const expense of expenses) {
+      if (expenses_by_person[expense.name]) {
+        expenses_by_person[expense.name] += expense.amount
+      } else {
+        expenses_by_person[expense.name] = expense.amount
+      }
+    }
+    sum = expenses.reduce((prev, expense) => prev + parseInt(expense.amount), 0)
+
+    equalShare = sum / Object.keys(expenses_by_person).length;
+
+    let owed_total = 0;
+
+    for (const name in expenses_by_person) {
+      if (expenses_by_person[name] < equalShare) {
+        owes.push({
+          name: name,
+          amount: expenses_by_person[name]
+        })
+      } else if (expenses_by_person[name] > equalShare) {
+        owed.push({
+          name: name,
+          amount: expenses_by_person[name]
+        })
+
+        owed_total += expenses_by_person[name];
+      }
+    }
+
+    console.log(owes, owed)
+    for(const owes_i of owes) {
+      const owe_amount = equalShare - owes_i['amount'];
+      for (const owed_i of owed) {
+        const amount = owed_i['amount'] / owed_total * owe_amount
+        payouts.push({
+          'owed': owed_i['name'],
+          'owes': owes_i['name'],
+          'amount': amount
+        })
+      }
+    }
   }
+
+
 
   let result_json = {
     total: sum,
-    equalShare: equalShare
+    equalShare: equalShare,
+    payouts: payouts
   }
   
   res.json(result_json);
